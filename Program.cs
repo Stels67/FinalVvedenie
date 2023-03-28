a81c6193bbe850b6d5631587a80debb2c1834b39
stels67 <melnikow.kirill@mail.ru>
2023-03-28 21:19:26 +0300

    Lesson23

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// 
+
+Console.Write("Введите длину массива - ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+string[] str = CreateArrayString(arrayLength);
+System.Console.Write("Исходный массив: ");
+PrintArray(str);
+System.Console.WriteLine();
+string[] newStr = NewArray(str);
+System.Console.Write("Новый массив: ");
+PrintArray(newStr);
+Console.ReadKey();
+
+
+
+void PrintArray(string[] array)
+{
+    for(int i = 0;i<array.Length;i++)
+    {
+        System.Console.Write($"\"{array[i]}\" ");
+    }
+    
+}
